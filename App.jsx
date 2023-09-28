3506da6e5a68892ff0733e352fb29ac337df20f4
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-09-28 13:31:19 +0200

    change of app structure

--- App.jsx
+++ App.jsx
@@ -38,123 +38,14 @@ export default function PlayerComponent() {
    return (
       <div className='video__container'>
         
-               {isFinished ? (
+         
                   <Spline
                      scene="https://prod.spline.design/wDnh46uuu0SbxmgA/scene.splinecode"
                      className='spline'
-                     style={{ display: isFinished ? 'block' : 'none' }}
                   />
-               ) : (
-                  <>
-                     {isPlaying ? (
-                        <ReactPlayer
-                           className="react-player"
-                           width="100%"
-                           height="100%"
-                           ref={playerRef}
-                           url={mijnFilm}
-                           controls={false}
-                           playing={true}
-                           onEnded={handleVideoEnded}
-                        />
-                     ) : (
-                        <>
-                           <Spline
-                              className="apple-spline"
-                              scene="https://prod.spline.design/qnzLCmcBfHFLcUyR/scene.splinecode"
-                           />
-                           <div className='button'>
-                              <button onClick={startMovie}>Enter the 3D experience</button>
-                           </div>
-                        </>
-                     )}
-                  </>
-               )}
 
       </div>
    );
    
 }
 
-
-// import React, { useRef, useState} from 'react';
-// import ReactPlayer from 'react-player';
-// import Spline from '@splinetool/react-spline';
-// import Animate from 'react-smooth';
-// import mijnFilm from './src/mijnFilm.mp4';
-// import useMediaQuery from '@mui/material/useMediaQuery';
-
-
-// export default function PlayerComponent() {
-
-//    const playerRef = useRef(null);
-//    const [isPlaying, setIsPlaying] = useState(false);
-//    const [isFinished, setIsFinished] = useState(false);
-
-//    const isTabletOrPhone = useMediaQuery('(max-width:768px)')
-//    console.log(isTabletOrPhone)
-
-//    function startMovie() {
-//       setIsPlaying((oldValue) => !oldValue);
-//    }
-
-
-//    const handleVideoEnded = () => {
-//       setIsFinished(true);
-//       if (playerRef.current) {
-//          playerRef.current.parentNode.removeChild(playerRef.current);
-//       }
-//    };
-
-//    return (
-//       <div className='video__container'>
-//          {isTabletOrPhone ? (
-//             <Spline
-//                scene="https://prod.spline.design/wDnh46uuu0SbxmgA/scene.splinecode"
-//                className='spline'
-//                style='block'
-//             />
-//          ) : (
-//             <>
-//                {isPlaying ? (
-//                   <ReactPlayer
-//                      className="react-player"
-//                      width="100%"
-//                      height="100%"
-//                      ref={playerRef}
-//                      url={mijnFilm}
-//                      controls={false}
-//                      playing={true}
-//                      onEnded={handleVideoEnded}
-//                   />
-//                ) : (
-//                   <>
-//                      <Spline
-//                         className="apple-spline"
-//                         scene="https://prod.spline.design/qnzLCmcBfHFLcUyR/scene.splinecode"
-//                      />
-//                      <div className='button'>
-//                         <button onClick={startMovie}>Enter the 3D experience</button> {/* Inhoud toegevoegd aan de knop */}
-//                      </div>
-//                   </>
-//                )}
-//             </>
-//          )}
-//          <Animate
-//             from={{ opacity: 0 }}
-//             to={{ opacity: 1 }}
-//             attributeName="opacity"
-//             duration={100} // Duur van de animatie in milliseconden
-//             delay={100} // Verliestijd voordat de animatie wordt gestart
-//          >
-//             {({ opacity }) => (
-//                <Spline
-//                   scene="https://prod.spline.design/wDnh46uuu0SbxmgA/scene.splinecode"
-//                   className='spline'
-//                   style={{ display: isFinished ? 'block' : 'none', opacity }}
-//                />
-//             )}
-//          </Animate>
-//       </div>
-//    );
-// }
