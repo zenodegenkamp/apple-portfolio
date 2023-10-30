aea219caac7d64ca4e921266883db3474ba56d12
zenodegenkamp <zenodegenkamp@hotmail.com>
2023-10-30 11:31:36 +0100

    change of spline logic

--- App.jsx
+++ App.jsx
@@ -1,31 +1,61 @@
-import React, { useRef, useState, useEffect} from 'react';
-import ReactPlayer from 'react-player';
-import Spline from '@splinetool/react-spline';
-import Animate from 'react-smooth';
-import mijnFilm from './src/mijnFilm.mp4';
-import useMediaQuery from '@mui/material/useMediaQuery';
+// import React, { useRef, useState, useEffect} from 'react';
+// import ReactPlayer from 'react-player';
+// import Spline from '@splinetool/react-spline';
+// import Animate from 'react-smooth';
+// import mijnFilm from './src/mijnFilm.mp4';
+// import useMediaQuery from '@mui/material/useMediaQuery';
 
+
+
+// export default function PlayerComponent() {
+
+
+//    return (
+      
+
+//     //         <iframe
+//     //   src='https://my.spline.design/test-c2a2ac943236cb867fee93ab947c05c6/'
+//     //   frameBorder='0'
+//     //   width='100%'
+//     //   height='100%'
+//     // />
+
+// <script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.490/build/spline-viewer.js"></script>
+// <spline-viewer hint loading-anim url="https://prod.spline.design/wDnh46uuu0SbxmgA/scene.splinecode"></spline-viewer>
+//    );
+   
+// }
+
+import React, { useEffect, useState } from 'react';
 
 
 export default function PlayerComponent() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/@splinetool/viewer@0.9.490/build/spline-viewer.js';
+    script.async = true;
+    document.body.appendChild(script);
 
-   return (
-      <div className='video__container'>
+    script.onload = () => {
+      const viewer = document.createElement('spline-viewer');
+      viewer.setAttribute('hint', 'loading-anim');
+      viewer.setAttribute('url', 'https://prod.spline.design/wDnh46uuu0SbxmgA/scene.splinecode');
+      document.body.appendChild(viewer);
 
-                  {/* <Spline
-                     scene="https://prod.spline.design/wDnh46uuu0SbxmgA/scene.splinecode"
-                     className='spline'
-                  /> */}
-            <iframe
-      src='https://my.spline.design/test-c2a2ac943236cb867fee93ab947c05c6/'
-      frameBorder='0'
-      width='100%'
-      height='100%'
-    />
+      // Markeer het laden als voltooid
+      setLoading(false);
+    };
 
-      </div>
-   );
-   
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div id="spline-viewer-container">
+      {loading ? <h1>Loading the magic world</h1> : null}
+    </div>
+  );
 }
-
